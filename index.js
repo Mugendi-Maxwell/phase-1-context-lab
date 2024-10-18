16b04d08d0d520c3e667f21181f21058ced9f012
Maxi-arch <mxmu025@gmail.com>
2024-10-18 21:40:04 +0300

    2ndcommit

--- index.js
+++ index.js
@@ -10,6 +10,7 @@
  */
 // Function to create an employee record
 
+// Function to create an employee record from an array
 function createEmployeeRecord(employeeArray) {
   return {
     firstName: employeeArray[0],
@@ -20,81 +21,90 @@ function createEmployeeRecord(employeeArray) {
     timeOutEvents: []
   };
 }
+
+// Function to create multiple employee records from an array of arrays
 function createEmployeeRecords(employeeArrays) {
   return employeeArrays.map(createEmployeeRecord);
 }
-function createTimeInEvent(employeeRecord, dateTime) {
+
+// Function to add a timeIn event to an employee record
+function createTimeInEvent(dateTime) {
   let [date, hour] = dateTime.split(' ');
   
-  employeeRecord.timeInEvents.push({
+  this.timeInEvents.push({
     type: "TimeIn",
     hour: parseInt(hour, 10),
     date: date
   });
   
-  return employeeRecord;
+  return this;
 }
-function createTimeOutEvent(employeeRecord, dateTime) {
+
+// Function to add a timeOut event to an employee record
+function createTimeOutEvent(dateTime) {
   let [date, hour] = dateTime.split(' ');
   
-  employeeRecord.timeOutEvents.push({
+  this.timeOutEvents.push({
     type: "TimeOut",
     hour: parseInt(hour, 10),
     date: date
   });
   
-  return employeeRecord;
+  return this;
 }
-function hoursWorkedOnDate(employeeRecord, date) {
-  let timeIn = employeeRecord.timeInEvents.find(event => event.date === date);
-  let timeOut = employeeRecord.timeOutEvents.find(event => event.date === date);
+
+// Function to calculate hours worked on a given date
+function hoursWorkedOnDate(date) {
+  let timeIn = this.timeInEvents.find(event => event.date === date);
+  let timeOut = this.timeOutEvents.find(event => event.date === date);
   
   return (timeOut.hour - timeIn.hour) / 100;
 }
-function wagesEarnedOnDate(employeeRecord, date) {
-  let hoursWorked = hoursWorkedOnDate(employeeRecord, date);
-  return hoursWorked * employeeRecord.payPerHour;
-}
-function wagesEarnedOnDate(employeeRecord, date) {
-  let hoursWorked = hoursWorkedOnDate(employeeRecord, date);
-  return hoursWorked * employeeRecord.payPerHour;
-}
-function allWagesFor(employeeRecord) {
-  let eligibleDates = employeeRecord.timeInEvents.map(event => event.date);
 
-  let totalWages = eligibleDates.reduce((total, date) => {
-    return total + wagesEarnedOnDate(employeeRecord, date);
+// Function to calculate wages earned on a specific date
+function wagesEarnedOnDate(date) {
+  let hoursWorked = hoursWorkedOnDate.call(this, date);
+  return hoursWorked * this.payPerHour;
+}
+
+// Function to calculate all wages for an employee
+function allWagesFor() {
+  let eligibleDates = this.timeInEvents.map(event => event.date);
+  
+  return eligibleDates.reduce((total, date) => {
+    return total + wagesEarnedOnDate.call(this, date);
   }, 0);
-
-  return totalWages;
 }
+
+// Function to find an employee by first name
 function findEmployeeByFirstName(srcArray, firstName) {
   return srcArray.find(employee => employee.firstName === firstName);
 }
+
+// Function to calculate the total payroll for an array of employee records
 function calculatePayroll(employeeRecords) {
   return employeeRecords.reduce((totalPayroll, employeeRecord) => {
-    return totalPayroll + allWagesFor(employeeRecord);
+    return totalPayroll + allWagesFor.call(employeeRecord);
   }, 0);
 }
 // Example employee data
 let employeeData = [
-  ["Thor", "Odinson", "God of Thunder", 100],
-  ["Loki", "Laufeyson", "God of Mischief", 50]
+  ["Gray", "Worm", "Security", 1],
+  ["John", "Doe", "Manager", 20]
 ];
 
 // Create employee records
 let employees = createEmployeeRecords(employeeData);
 
 // Add time events
-createTimeInEvent(employees[0], "2024-10-18 0900");
-createTimeOutEvent(employees[0], "2024-10-18 1700");
+createTimeInEvent.call(employees[0], "2024-10-18 0900");
+createTimeOutEvent.call(employees[0], "2024-10-18 1700");
+createTimeInEvent.call(employees[1], "2024-10-18 1000");
+createTimeOutEvent.call(employees[1], "2024-10-18 1600");
 
-createTimeInEvent(employees[1], "2024-10-18 1000");
-createTimeOutEvent(employees[1], "2024-10-18 1600");
-
-// Calculate payroll
-let payroll = calculatePayroll(employees);
-console.log(payroll);  // Output total payroll
+// Calculate total payroll
+let totalPayroll = calculatePayroll(employees);
+console.log(totalPayroll); // Outputs the total payroll
 
 
 
